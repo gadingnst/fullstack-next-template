7ca5e488d81a251da28edf03e5c4de5ce35b5ff7
Gading Nasution <sutan.gnst@gmail.com>
2022-10-31 23:00:51 +0700

    rename: folder LazyLoad

---
+++ src/components/base/LazyLoad/index.tsx
@@ -0,0 +1,19 @@
+import type { FunctionComponent, PropsWithChildren } from 'react';
+import { LazyLoadComponent, LazyLoadComponentProps, trackWindowScroll } from 'react-lazy-load-image-component';
+
+export type Props = LazyLoadComponentProps;
+
+/** @see https://www.npmjs.com/package/react-lazy-load-image-component#using-trackwindowscroll-hoc-to-improve-performance */
+export { trackWindowScroll };
+
+/** @see https://www.npmjs.com/package/react-lazy-load-image-component#lazyloadcomponent-usage */
+const LazyLoad: FunctionComponent<PropsWithChildren<Props>> = (props) => {
+  const { children, ...otherProps } = props;
+  return (
+    <LazyLoadComponent {...otherProps}>
+      {children}
+    </LazyLoadComponent>
+  );
+};
+
+export default LazyLoad;
