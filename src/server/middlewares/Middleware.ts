41b941a4fe2d9da19af81129c42599474b846277
Gading Nasution <account@gading.dev>
2023-06-01 14:05:19 +0700

    refactor middleware wrapper

--- src/server/middlewares/Middleware.ts
+++ src/server/middlewares/Middleware.ts
@@ -1,8 +1,8 @@
-export type MethodHandler = (req: Request) => void;
 export type NextFunction = () => void;
+export type Handler = (req: Request) => void;
 export type MiddlewareHandler = (req: Request, next: NextFunction) => void;
 
-const withMiddleware = (handler: MiddlewareHandler) => (next: MethodHandler) => (req: Request) => {
+const withMiddleware = (handler: MiddlewareHandler) => (next: Handler) => (req: Request) => {
   const nextHandler = () => next(req);
   return handler(req, nextHandler);
 };
