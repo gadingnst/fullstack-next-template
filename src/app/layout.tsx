de4458e2d478353dfe435d14cdf4c3d7f5601c41
Gading Nasution <account@gading.dev>
2023-07-13 00:38:48 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,26 @@ import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { FC, PropsWithChildren } from 'react';
 import { SITE_NAME } from '@/configs/env';
+import { withMetadata } from '@/packages/utils/metadata';
+
 import GitHubBadge from '@/packages/components/base/Floatings/GithubBadge';
 
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter', adjustFontFallback: false });
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  adjustFontFallback: false
+});
 
 /**
  * HTML Metadata in App Route
  * @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata#basic-fields
  */
-export const metadata = {
+export const metadata = withMetadata({
   title: {
     default: SITE_NAME,
     template: `%s | ${SITE_NAME}`
   }
-} satisfies Metadata;
+});
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
