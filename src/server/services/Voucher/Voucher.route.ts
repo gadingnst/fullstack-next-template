766fae95d396eb9166ae350a7ccb232040da55a5
Gading Nasution <account@gading.dev>
2023-07-16 12:42:01 +0700

    Update Voucher.route.ts

--- src/server/services/Voucher/Voucher.route.ts
+++ src/server/services/Voucher/Voucher.route.ts
@@ -1,4 +1,4 @@
-import VoucherHandler from '@/server/services/Voucher/Voucher.controller';
+import VoucherController from '@/server/services/Voucher/Voucher.controller';
 import withVerifyAppKey from '@/server/packages/middlewares/withVerifyAppKey';
 
 /**
@@ -6,5 +6,5 @@ import withVerifyAppKey from '@/server/packages/middlewares/withVerifyAppKey';
  * @dir `app/api/vouchers/route.ts`
  */
 
-export const GET = VoucherHandler.index;
-export const POST = withVerifyAppKey(VoucherHandler.insert);
+export const GET = VoucherController.index;
+export const POST = withVerifyAppKey(VoucherController.insert);
